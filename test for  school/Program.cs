8176c440b394258d2f236a6cf0e2a29853d67e5f
Somber <dj2548@gmail.com>
2022-09-26 13:46:51 -0400

    inital commit

---
+++ test for  school/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+bool run = true;
+
+while (true)
+{
+
+    Console.WriteLine(" Please input the radius of a circle");
+    string input = Console.ReadLine();
+    double radius = double.Parse(input);
+
+    Console.WriteLine(" Radius: " + radius);
+
+    double area = Math.PI * radius * radius;
+
+    Console.WriteLine(" Area " + area);
+
+    Console.WriteLine("\n");
+    Console.WriteLine(" Would you like to measuse another circle?");
+    input = Console.ReadLine();
+    if (input == "y" || input == "yes")
+    { 
+    } else {
+        Console.WriteLine("Goodbye");
+        }
+}
